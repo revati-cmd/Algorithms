dee5e06f8eba087048b519fa4358d9de6712691c
revati-cmd <62731631+revati-cmd@users.noreply.github.com>
2020-03-26 19:00:52 -0700

    Create FirstUniqueCharacterInString.java

---
+++ FirstUniqueCharacterInString.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap <Character, Integer> hMap = new HashMap <Character, Integer>();
+        for(int i=0; i<s.length();i++) {
+            char currentChar = s.charAt(i);
+           // hMap.put(currentChar, hMap.getOrDefault(currentChar,-2)+1)
+            
+            if(hMap.containsKey(currentChar)) {
+                hMap.put(currentChar, -1); //unique char
+            }
+            else {
+                hMap.put(currentChar, i); //not unique
+            }
+        }
+        for(int i=0; i<s.length();i++)
+            if(hMap.get(s.charAt(i))!= -1)
+                return hMap.get(s.charAt(i));
+        
+        return -1;
+    }
+}
